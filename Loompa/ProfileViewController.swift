f11f1869aabc9d677299c85965b4d5a66f165b52
Brian <swiftapprentice@gmail.com>
2020-01-17 11:11:34 -0600

    Added Views and Code Files

---
+++ Loompa/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Loompa
+//
+//  Created by Brian Griffin on 1/17/20.
+//  Copyright © 2020 swiftapprentice. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Loompa/SearchBarView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchBarView.swift
+//  Loompa
+//
+//  Created by Brian Griffin on 1/17/20.
+//  Copyright © 2020 swiftapprentice. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
