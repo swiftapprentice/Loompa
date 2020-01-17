3a5558f78051527eb6712be07a93612d9824d784
Brian <swiftapprentice@gmail.com>
2020-01-17 11:07:11 -0600

    First Commit

---
+++ Loompa/MyCustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCustomCell.swift
+//  Loompa
+//
+//  Created by Brian Griffin on 1/17/20.
+//  Copyright © 2020 swiftapprentice. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
